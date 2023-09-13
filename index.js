c185c52ccba1b9e3fe011dfbf01e7189dbbb5012
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-09-14 01:37:46 +0200

    hw1

---
+++ index.js
@@ -0,0 +1,28 @@
+const argv = require("yargs").argv;
+const contacts = require("./contacts");
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContact = await contacts.listContacts();
+      return console.log(allContact);
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact({ id } );
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
